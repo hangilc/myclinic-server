d382d1b2ecf5dbcd9cab6171a497ba0b6b2c059c
hangilc <hangil@chang.jp>
2016-10-26 17:07:15 +0900

    modified index.js

--- index.js
+++ index.js
@@ -2,25 +2,39 @@
 
 var express = require("express");
 var bodyParser = require("body-parser");
-
+var config = require("myclinic-config").create();
 var app = express();
 
 var subs = [
 	{ 
 		name: "service",
-		package: "myclinic-service"
+		package: "myclinic-service",
+		config: {
+			dbConfig: config.get("database"),
+			masterMap: config.get("master-map"),
+			nameMap: config.get("master-name"),
+			houkatsuList: config.get("rcpt-houkatsu")
+		}
 	},
 	{
 		name: "practice",
-		package: "myclinic-practice"
+		package: "myclinic-practice",
+		config: {}
 	},
 	{
 		name: "shohousen",
-		package: "myclinic-shohousen"
+		package: "myclinic-shohousen",
+		config: Object.assign({}, config.get("shohousen"), {
+			"print-server-port": config.get("print-server-port")
+		})
 	},
 	{
 		name: "refer",
-		package: "myclinic-refer"
+		package: "myclinic-refer",
+		config: Object.assign({}, config.get("refer"), {
+			predefined: config.get("refer-predefined"),
+			"print-server-port": config.get("print-server-port")
+		})
 	}
 ];
 
@@ -28,13 +42,12 @@ subs.forEach(function(sub){
 	var name = sub.name;
 	var pkgName = sub.package;
 	var pkg = require(pkgName);
-	var config = require("./config/" + name + "-config");
+	var config = sub.config;
 	var subApp = express();
 	subApp.use(bodyParser.urlencoded({extended: false}));
 	subApp.use(bodyParser.json());
 	pkg.initApp(subApp, config);
 	if( pkg.staticDir ){
-		console.log(pkg.staticDir);
 		subApp.use(express.static(pkg.staticDir));
 	}
 	app.use("/" + name, subApp);
